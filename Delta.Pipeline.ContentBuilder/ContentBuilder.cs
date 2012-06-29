ca6771d1c7b18026e516826c9027b3591c935281
Lucas Stranks <lithiumtoast@live.com>
2012-06-29 10:50:19 -0700

    Delta.Pipeline.ContentBuilder

---
+++ Delta.Pipeline.ContentBuilder/ContentBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Delta
+{
+    public class ContentBuilder
+    {
+        string _contentProject = string.Empty;
+
+        public LoggerVerbosity LoggerVerbosity { get; set; }
+        public string OutputPath { get; set; }
+        public string ContentRootDirectory { get; set; }
+        public GraphicsProfile GraphicsProfile { get; set; }
+        public TargetPlatform TargetPlatform { get; set; }
+        public bool CompressContent { get; set; }
+
+        //public static string DebuggingTargets
+        //{
+        //    get
+        //    {
+        //        if (String.IsNullOrEmpty(SingleItem))
+        //        {
+        //            return String.Empty;
+        //        }
+
+        //        string targetsPath = @"Targets\Debugging.targets";
+        //        return Path.Combine(BuildToolDirectory, targetsPath);
+        //    }
+        //}
+
+        public ContentBuilder(string contentProjectFile)
+        {
+            FileInfo fi = new FileInfo(contentProjectFile);
+            if (fi.Extension != ".contentproj")
+                throw new NotSupportedException(string.Format("The file '{0}' is not a XNA C# content project.", contentProjectFile));
+            if (!fi.Exists)
+                throw new FileNotFoundException(String.Format("The file '{0}' does not exist.", contentProjectFile), contentProjectFile);
+            _contentProject = Path.GetFullPath(contentProjectFile);
+            LoggerVerbosity = LoggerVerbosity.Normal;
+            GraphicsProfile = GraphicsProfile.HiDef;
+            TargetPlatform = TargetPlatform.Windows;
+            CompressContent = false;
+        }
+
+        public bool Build()
+        {
+            string originalWorkingDirectory = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Path.GetDirectoryName(_contentProject);
+            var globalProperties = new Dictionary<string, string>();
+            globalProperties.Add("XnaProfile", GraphicsProfile.ToString());
+            globalProperties.Add("XNAContentPipelineTargetPlatform", TargetPlatform.ToString());
+            globalProperties.Add("XnaCompressContent", CompressContent.ToString());
+            globalProperties.Add("OutputPath", OutputPath);
+            globalProperties.Add("ContentRootDirectory", ContentRootDirectory);
+            //globalProperties.Add("CustomAfterMicrosoftCommonTargets", DebuggingTargets);
+            var project = ProjectCollection.GlobalProjectCollection.LoadProject(_contentProject, globalProperties, "4.0");
+            bool r = project.Build(new ConsoleLogger(LoggerVerbosity));
+            Environment.CurrentDirectory = originalWorkingDirectory;
+            return r;
+        }
+
+    }
+}
---
+++ Delta.Pipeline.ContentBuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Delta.Pipeline.ContentBuilder")]
+[assembly: AssemblyProduct("Delta.Pipeline.ContentBuilder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("30964cf1-d6c9-4ef1-8ec5-48b0d464f4cc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
