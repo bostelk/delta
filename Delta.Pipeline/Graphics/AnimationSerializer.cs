b54a8b47d18de993a98f3ea10791de8bc56e1eee
Lucas Stranks <lucas@narwhunderful.com>
2012-07-08 15:59:43 -0700

    Added spritesheet importer or .spritesheet files. Ensures that newly added spritesheets automatically are signed the right processor.

--- Delta.Pipeline/Graphics/AnimationSerializer.cs
+++ Delta.Pipeline/Graphics/AnimationSerializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.IO;
 using System.ComponentModel;
 using System.Collections.Generic;
---
+++ Delta.Pipeline/Graphics/SpriteSheetImporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace Delta.Graphics
+{
+    [ContentImporter(".spritesheet", DisplayName = "SpriteSheetImporter", DefaultProcessor = "SpriteSheetProcessor")]
+    public class SpriteSheetImporter : ContentImporter<SpriteSheetContent>
+    {
+        public override SpriteSheetContent Import(string fileName, ContentImporterContext context)
+        {
+            XmlImporter xmlImporter = new XmlImporter();
+            return xmlImporter.Import(fileName, context) as SpriteSheetContent;
+        }
+    }
+}
