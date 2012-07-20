a45d68d01f6d0d6a0c5ed03aef8e14439117e50c
Lucas Stranks <lucas@narwhunderful.com>
2012-07-19 22:25:48 -0700

    My to do list.

--- Delta.Core/UI/Screens/DebugScreen.cs
+++ Delta.Core/UI/Screens/DebugScreen.cs
@@ -14,8 +14,8 @@ namespace Delta.UI
             _performanceMetrics.ForeColor = Color.White;
             _performanceMetrics.BackColor = Color.Gray;
             Add(_performanceMetrics);
-            Label lbl = new Label() { BackColor = Color.Gray, Position = new Vector2(50, 50), Size = new Vector2(300, 200), AutoSize = false, IsWordWrapped = true};
-            lbl.Text.Append("Hi, this is a really long string that I hope gets wrapped properly when I test it. \n Custom new line. \n Kyle seems made at me cause I'm taking forever to get this done. Whatever, I don't care. I have to go through and clip each control by it's bounding area so text will never be rendered outside the control's bounds. For now, it's good enough. I'll look into it more in alittle bit.");
+            Label lbl = new Label() { BackColor = Color.Gray, Position = new Vector2(900, 50), Size = new Vector2(300, 200), AutoSize = false, IsWordWrapped = true};
+            lbl.Text.Append("*yawn* \n//Lucas's To Do List:\n1. Don't append any render text beyond the border.\n2. Clip each control by starting a new SpriteBatch.Begin() for each control.\n3. Ignore Textbox for now, lol.\n4. Screen transitions.");
             lbl.Invalidate();
             Add(lbl);
             //Add(new Controls.Textbox() { BackColor = Color.Gray, Position = new Vector2(50, 50), Size = new Vector2(100, 100), AutoSize = false, VerticalTextAlignment = Controls.VerticalTextAlignment.Center, HorizontalTextAlignment = Controls.HorizontalTextAlignment.Center, WordWrap = true});
