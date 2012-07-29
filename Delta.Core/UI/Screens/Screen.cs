3e4fdcb0655e7b04afdba55830c83cf9dd176e2b
Lucas Stranks <lucas@narwhunderful.com>
2012-07-29 11:41:41 -0700

    Git is fucky.

--- Delta.Core/UI/Screens/Screen.cs
+++ Delta.Core/UI/Screens/Screen.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Delta.UI.Controls;
-using Delta.UI.Screens.Transitions;
+//using Delta.UI.Screens.Transitions;
 
 namespace Delta.UI.Screens
 {
@@ -15,11 +15,11 @@ namespace Delta.UI.Screens
 
     public abstract class Screen : EntityParent<Control>
     {
-        Transition _currentTransition = null;
+        //Transition _currentTransition = null;
 
         public ScreenState State { get; internal set; }
-        public Transition OnTransition { get; set; }
-        public Transition OffTransition { get; set; }
+        //public Transition OnTransition { get; set; }
+        //public Transition OffTransition { get; set; }
 
         public Screen()
             : base()
@@ -67,46 +67,46 @@ namespace Delta.UI.Screens
         protected override void LightUpdate(DeltaGameTime time)
         {
             base.LightUpdate(time);
-            if (_currentTransition == null)
-            {
-                if (State == ScreenState.TransitionOn)
-                    _currentTransition = OnTransition;
-                else if (State == ScreenState.TransitionOff)
-                    _currentTransition = OffTransition;
-                else
-                    return;
-            }
-            if (_currentTransition == null)
-            {
-                State = ScreenState.None;
-                return;
-            }
-            if (_currentTransition.Progress <= 0)
-                OnTransitionStarted();
-            _currentTransition.Progress = (time.ElapsedSeconds / _currentTransition.Time * 100).Clamp(0, 100);
-            if (_currentTransition.Progress == 100)
-            {
-                OnTransitionFinished();
-                State = ScreenState.None;
-                _currentTransition = null;
-                return;
-            }
-            _currentTransition.InternalUpdate(time);
+            //if (_currentTransition == null)
+            //{
+            //    if (State == ScreenState.TransitionOn)
+            //        _currentTransition = OnTransition;
+            //    else if (State == ScreenState.TransitionOff)
+            //        _currentTransition = OffTransition;
+            //    else
+            //        return;
+            //}
+            //if (_currentTransition == null)
+            //{
+            //    State = ScreenState.None;
+            //    return;
+            //}
+            //if (_currentTransition.Progress <= 0)
+            //    OnTransitionStarted();
+            //_currentTransition.Progress = (time.ElapsedSeconds / _currentTransition.Time * 100).Clamp(0, 100);
+            //if (_currentTransition.Progress == 100)
+            //{
+            //    OnTransitionFinished();
+            //    State = ScreenState.None;
+            //    _currentTransition = null;
+            //    return;
+            //}
+            //_currentTransition.InternalUpdate(time);
         }
 
         protected override void Draw(DeltaGameTime time, SpriteBatch spriteBatch)
         {
             base.Draw(time, spriteBatch);
-            if (_currentTransition != null)
-                _currentTransition.InternalDraw(time, spriteBatch);
+            //if (_currentTransition != null)
+            //    _currentTransition.InternalDraw(time, spriteBatch);
         }
 
         public void Exit()
         {
-            if (OffTransition == null || OffTransition.Time <= 0)
-                ExitImmediately();
-            else
-                State = ScreenState.TransitionOff;
+            //if (OffTransition == null || OffTransition.Time <= 0)
+            //    ExitImmediately();
+            //else
+            //    State = ScreenState.TransitionOff;
         }
 
         public void ExitImmediately()
@@ -120,7 +120,7 @@ namespace Delta.UI.Screens
         protected override void OnBeginDraw(DeltaGameTime time, SpriteBatch spriteBatch)
         {
             base.OnBeginDraw(time, spriteBatch);
-            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, SpriteBatchExtensions._cullRasterizerState, null, _currentTransition == null ? Matrix.Identity : _currentTransition.View);
+            //spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, SpriteBatchExtensions._cullRasterizerState, null, _currentTransition == null ? Matrix.Identity : _currentTransition.View);
         }
 
         protected override void OnEndDraw(DeltaGameTime time, SpriteBatch spriteBatch)
@@ -131,7 +131,7 @@ namespace Delta.UI.Screens
 
         protected virtual void OnTransitionStarted()
         {
-            To DO //MAKE ON/OFF TRANSITIONS ENCAPSULATE ONE CLASS
+            //To DO //MAKE ON/OFF TRANSITIONS ENCAPSULATE ONE CLASS
                 // use basiceffect? We want to alpha things/out
         }
 
